be0e9d3ae283a0caf58a23561b6a48e4f409ebe2
Harrison Healey <harrisonmhealey@gmail.com>
2019-03-08 10:28:56 -0500

    Initial commit

---
+++ server/manifest.go
@@ -0,0 +1,9 @@
+package main
+
+var manifest = struct {
+	Id      string
+	Version string
+}{
+	Id:      "com.mattermost.nps-plugin",
+	Version: "0.0.1",
+}
---
+++ webapp/src/manifest.js
@@ -0,0 +1,2 @@
+export const id = 'com.mattermost.nps-plugin';
+export const version = '0.0.1';
