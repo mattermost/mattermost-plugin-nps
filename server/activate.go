9eec62c8fa8e74714917387c8514b286d99fc8c0
Hanzei <hanzei@mailbox.org>
2019-03-22 19:54:04 +0100

    Load user profile picture from file

--- server/activate.go
+++ server/activate.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/blang/semver"
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/pkg/errors"
@@ -54,36 +57,44 @@ func (p *Plugin) canSendDiagnostics() bool {
 
 func (p *Plugin) ensureBotExists() error {
 	// Attempt to find an existing bot
-	botUserIdBytes, err := p.API.KVGet(BOT_USER_KEY)
-	if err != nil {
-		return err
+	botUserIdBytes, appErr := p.API.KVGet(BOT_USER_KEY)
+	if appErr != nil {
+		return appErr
 	}
 
 	if botUserIdBytes == nil {
 		// Create a bot since one doesn't exist
 		p.API.LogDebug("Creating bot for NPS plugin")
 
-		bot, err := p.API.CreateBot(&model.Bot{
+		bundlePath, err := p.API.GetBundlePath()
+		if err != nil {
+			return errors.Wrap(err, "failed to get bundle path")
+		}
+
+		profileImage, err := ioutil.ReadFile(filepath.Join(bundlePath, "assets", "icon-happy-bot-square@1x.png"))
+		if err != nil {
+			return errors.Wrap(err, "failed to read profile image")
+		}
+
+		bot, appErr := p.API.CreateBot(&model.Bot{
 			Username:    "surveybot",
 			DisplayName: "Surveybot",
 			Description: "Created by the Net Promoter Score plugin.",
 		})
-		if err != nil {
-			return err
+		if appErr != nil {
+			return appErr
 		}
 
 		// Give it a profile picture
-		err = p.API.SetProfileImage(bot.UserId, profileImage)
-		if err != nil {
-			p.API.LogError("Failed to set profile image for bot", "err", err)
+		if appErr = p.API.SetProfileImage(bot.UserId, profileImage); appErr != nil {
+			p.API.LogError("Failed to set profile image for bot", "error", appErr)
 		}
 
 		p.API.LogDebug("Bot created for NPS plugin")
 
 		// Save the bot ID
-		err = p.API.KVSet(BOT_USER_KEY, []byte(bot.UserId))
-		if err != nil {
-			return err
+		if appErr = p.API.KVSet(BOT_USER_KEY, []byte(bot.UserId)); appErr != nil {
+			return appErr
 		}
 
 		p.botUserId = bot.UserId
