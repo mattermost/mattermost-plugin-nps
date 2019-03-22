3661fa31544e4aa88618f728a9c309b92f517a65
Harrison Healey <harrisonmhealey@gmail.com>
2019-03-22 10:19:11 -0400

    MM-14340 Send written feedback to Segment  (#12)
    * MM-14340 Send written feedback to Segment
    
    * Move diagnostics check to start of MessageWillBePosted hook

--- server/hooks.go
+++ server/hooks.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"strings"
-
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
 )
 
 func (p *Plugin) ChannelHasBeenCreated(c *plugin.Context, channel *model.Channel) {
 	// Set the description for any DM channels opened between Surveybot and a user
-	if channel.Type != model.CHANNEL_DIRECT {
-		return
-	}
-
-	if !strings.HasPrefix(channel.Name, p.botUserId+"__") && !strings.HasSuffix(channel.Name, "__"+p.botUserId) {
+	if !p.isBotDMChannel(channel) {
 		return
 	}
 
@@ -21,5 +15,38 @@ func (p *Plugin) ChannelHasBeenCreated(c *plugin.Context, channel *model.Channel
 
 	if _, err := p.API.UpdateChannel(channel); err != nil {
 		p.API.LogWarn("Failed to set channel header for Surveybot", "err", err)
+	}
+}
+
+func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
+	if !p.canSendDiagnostics() {
+		return
+	}
+
+	if post.UserId == p.botUserId {
+		return
+	}
+
+	// Respond to any written feedback that the bot receives
+	channel, err := p.API.GetChannel(post.ChannelId)
+	if err != nil {
+		p.API.LogWarn("Unable to get channel for post to send Surveybot response", "err", err)
+		return
+	}
+
+	if !p.isBotDMChannel(channel) {
+		return
+	}
+
+	// Send the feedback to Segment
+	p.sendFeedback(post.Message, post.UserId, post.CreateAt)
+
+	// Respond to the feedback
+	_, err = p.CreateBotDMPost(post.UserId, &model.Post{
+		Message: ":tada: Thanks for helping improve Mattermost!",
+		Type:    "custom_nps_thanks",
+	})
+	if err != nil {
+		p.API.LogError("Failed to respond to Surveybot feedback")
 	}
 }
