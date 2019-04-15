2b38efc7e73550533fd19177c2f939efdef05233
Harrison Healey <harrisonmhealey@gmail.com>
2019-04-15 09:28:11 -0400

    MM-14804 Set CSRF token from cookie

--- webapp/src/client.js
+++ webapp/src/client.js
@@ -1,20 +1,25 @@
 import {id as pluginId} from './manifest';
 
+const HEADER_X_CSRF_TOKEN = 'X-CSRF-Token';
+
 export class Client {
     constructor() {
         this.url = `/plugins/${pluginId}/api/v1`;
     }
 
     connected = () => {
-        return this.doFetch(`${this.url}/connected`, {
-            method: 'POST',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-        });
+        return this.doFetch(`${this.url}/connected`, {method: 'POST'});
     }
 
     doFetch = async (url, options) => {
+        if (!options.headers) {
+            options.headers = {};
+        }
+
+        if (options.method && options.method.toLowerCase() !== 'get') {
+            options.headers[HEADER_X_CSRF_TOKEN] = getCSRFFromCookie();
+        }
+
         try {
             const response = await fetch(url, options);
             const data = await response.json();
@@ -24,4 +29,15 @@ export class Client {
             return {error};
         }
     }
+}
+
+function getCSRFFromCookie() {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        if (cookie.startsWith('MMCSRF=')) {
+            return cookie.replace('MMCSRF=', '');
+        }
+    }
+
+    return '';
 }
