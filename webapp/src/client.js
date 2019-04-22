3f059accefe7a139460a4ebad9d15c4b0c96300e
Harrison Healey <harrisonmhealey@gmail.com>
2019-04-22 15:30:52 -0400

    MM-14804 Set CSRF token from cookie (#29)
    * MM-14804 Set CSRF token from cookie
    
    * Re-add X-Requested-With header
    
    * Only get CSRF token from cookie once

--- webapp/src/client.js
+++ webapp/src/client.js
@@ -1,20 +1,28 @@
 import {id as pluginId} from './manifest';
 
+const HEADER_X_CSRF_TOKEN = 'X-CSRF-Token';
+
 export class Client {
     constructor() {
+        this.csrf = getCSRFFromCookie();
         this.url = `/plugins/${pluginId}/api/v1`;
     }
 
     connected = () => {
-        return this.doFetch(`${this.url}/connected`, {
-            method: 'POST',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-        });
+        return this.doFetch(`${this.url}/connected`, {method: 'POST'});
     }
 
     doFetch = async (url, options) => {
+        if (!options.headers) {
+            options.headers = {};
+        }
+
+        options.headers['X-Requested-With'] = 'XMLHttpRequest';
+
+        if (options.method && options.method.toLowerCase() !== 'get') {
+            options.headers[HEADER_X_CSRF_TOKEN] = this.csrf;
+        }
+
         try {
             const response = await fetch(url, options);
             const data = await response.json();
@@ -24,4 +32,15 @@ export class Client {
             return {error};
         }
     }
+}
+
+function getCSRFFromCookie() {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        if (cookie.startsWith('MMCSRF=')) {
+            return cookie.replace('MMCSRF=', '');
+        }
+    }
+
+    return '';
 }
