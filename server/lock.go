7a367f02115faa3bd452ee1196716c4f30a03439
Gervasio Marchand <github@gervas.io>
2019-08-06 14:41:41 -0300

    Use KVCompareAndDelete to release the lock
    https://github.com/mattermost/mattermost-server/pull/11804

--- server/lock.go
+++ server/lock.go
@@ -63,18 +63,8 @@ func (p *Plugin) clearStaleLocks(now time.Time) *model.AppError {
 			_ = json.Unmarshal(value, &t)
 
 			if now.Sub(t) >= LOCK_EXPIRATION {
-				// There's no KVCompareAndDelete, so make sure we're the only ones modifying this lock
-				set, err := p.API.KVCompareAndSet(key, value, []byte("releasing"))
+				_, err := p.API.KVCompareAndDelete(key, value)
 				if err != nil {
-					return err
-				} else if !set {
-					// Someone else has released or cleared the lock
-					continue
-				}
-
-				p.API.LogInfo("Freeing expired lock", "key", key)
-
-				if err := p.API.KVDelete(key); err != nil {
 					return err
 				}
 			}
