9069c2689df9497947ef858287b38d77073456c9
mickmister <mjkochell@gmail.com>
2019-11-20 13:31:08 -0500

    update mattermost-redux

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -20,8 +20,22 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: [
-                            '@babel/env',
-                            '@babel/preset-react',
+                            ['@babel/preset-env', {
+                                targets: {
+                                    chrome: 66,
+                                    firefox: 60,
+                                    edge: 42,
+                                    safari: 12,
+                                },
+                                corejs: 3,
+                                modules: false,
+                                debug: false,
+                                useBuiltIns: 'usage',
+                                shippedProposals: true,
+                            }],
+                            ['@babel/preset-react', {
+                                useBuiltIns: true,
+                            }],
                         ],
                         plugins: [
                             '@babel/plugin-proposal-class-properties',
