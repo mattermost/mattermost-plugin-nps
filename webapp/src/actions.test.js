6883672c41a103f183b086a76759c75418fee518
Julien Tant <julien@craftyx.fr>
2022-02-10 17:36:43 -0700

    add test for actions.js

--- webapp/src/actions.test.js
+++ webapp/src/actions.test.js
@@ -1,4 +1,4 @@
-import {connected} from './actions';
+import {connected, userWantsToGiveFeedback} from './actions';
 
 describe('connected', () => {
     const client = {
@@ -31,5 +31,46 @@ describe('connected', () => {
         connected(client)(null, getState);
 
         expect(client.connected).not.toHaveBeenCalled();
+    });
+});
+
+describe('userWantsToGiveFeedback', () => {
+    const client = {
+        userWantsToGiveFeedback: jest.fn(),
+    };
+
+    test('should do nothing if the user is not logged in', () => {
+        const getState = () => ({
+            entities: {
+                users: {
+                    currentUserId: '',
+                },
+            },
+        });
+
+        userWantsToGiveFeedback(client)(null, getState);
+
+        expect(client.userWantsToGiveFeedback).not.toHaveBeenCalled();
+    });
+
+    test('should call user wants feedback API if the user is logged in', () => {
+        jest.mock('./browser_routing');
+        jest.mock('mattermost-redux/selectors/entities/channels');
+
+        const getState = () => ({
+            entities: {
+                users: {
+                    currentUserId: 'user1',
+                },
+            },
+        });
+        const mockThen = jest.fn();
+        client.userWantsToGiveFeedback.mockImplementationOnce(() => {
+            return {then: mockThen};
+        });
+
+        userWantsToGiveFeedback(client)(null, getState);
+        expect(client.userWantsToGiveFeedback).toHaveBeenCalled();
+        expect(mockThen).toHaveBeenCalled();
     });
 });
