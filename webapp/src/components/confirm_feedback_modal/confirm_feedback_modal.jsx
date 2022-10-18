dde7bfb5c1e7e9f0e61b657ceb0d173edac94f9f
Julien Tant <julien@craftyx.fr>
2022-10-18 13:37:41 -0700

    wording

--- webapp/src/components/confirm_feedback_modal/confirm_feedback_modal.jsx
+++ webapp/src/components/confirm_feedback_modal/confirm_feedback_modal.jsx
@@ -50,7 +50,7 @@ export default class ConfirmFeedbackModal extends React.PureComponent {
                     <Modal.Title>{'Send feedback'}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>{'You are about to send feedback about Mattermost. Are you sure?'}</p>
+                    <p>{'You\'re about to send feedback about Mattermost.'}</p>
                     <p><strong>{'Optional'}</strong>{': If you\'d like to be contacted by our team, include your email address.'}</p>
                     <div className='form-group'>
                         <input
